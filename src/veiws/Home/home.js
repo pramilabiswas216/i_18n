a49cc9b5e64622b0eafa5c1ff83504317b236531
pramilabiswas <pramilabiswas216@gmail.com>
2024-04-02 00:00:15 +0530

    I18n is completed

--- src/veiws/Home/home.js
+++ src/veiws/Home/home.js
@@ -4,18 +4,26 @@ import I18n from '../../utils/i_18n';
 
 function Home() {
 
-    
+   const userCount = 100; 
 
   return (
+
+    
     <div>
+
+      <h1>Rode to Code</h1>
       <h1>{I18n("welcomeMessage")}</h1>
 
       <p>{I18n("descriptionMessage")}</p>
 
+      <h5>{I18n("userStateMessage", "<studentCount>", userCount)}</h5>
+
       <h3>{I18n("greetingMessage")}</h3>
 
+      <h3>{I18n("thanksMessage")}</h3>
 
 
+       
        
 
       <select defaultValue={localStorage.getItem("lang")} onChange={(e)=>{
